db8fb0f7d67642d48d556156fb56e6daacb91da8
Filip Cieslik <filip.cieslik@vattenfall.com>
2021-02-19 13:28:50 +0100

    View fix

--- scripts/more-panel.js
+++ scripts/more-panel.js
@@ -6,7 +6,6 @@ const moreBtn = more.querySelector('.more__btn-wrapper');
 const moreBtnTxt = more.querySelector('.more__btn-text');
 
 function handleLessBtnClick() {
-  console.log('less');
   morePanel.classList.remove('open');
   more.classList.remove('more-open');
   moreBtnTxt.textContent = 'MORE';
@@ -15,7 +14,6 @@ function handleLessBtnClick() {
 }
 
 function handleMoreBtnClick() {
-  console.log('more');
   morePanel.classList.add('open');
   more.classList.add('more-open');
   moreBtnTxt.textContent = 'LESS';
